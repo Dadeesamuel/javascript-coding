d2152f73fc132bec9d2af9f554776c6c3195fd47
DADEE SAMUEL <dadeesamuel@gmail.com>
2021-10-10 08:54:15 +0100

    promting the user

---
+++ javascript.js
@@ -0,0 +1,8 @@
+let username = prompt("enter your name")
+alert1 = alert("hello" + " " + username + " " + "kindly read the sentence in the next prompt and input the appropriate answer for the command")
+alert2 = alert("There was once a _________ programmer who wanted to use Javascript to ___________ the __________ ");
+let adjective = prompt("select an adjective");
+let verb = prompt("select a verb ");
+let noun = prompt("select a noun");
+alert3 = alert("kindly hold on while we process your answer");
+alert4 = alert("There was once a " + " " + adjective + " " + "programmer who wanted to use Javascript to" + " " + verb + " " + "the" + " " + noun);
