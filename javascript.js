a72347987f8ecd79fee8a3aa289896a2a593275b
DADEE SAMUEL <dadeesamuel@gmail.com>
2021-10-11 10:24:16 +0100

    adding a console

--- javascript.js
+++ javascript.js
@@ -1,9 +1,10 @@
-let username = prompt("enter your name")
+let username = prompt("enter your name");
 alert1 = alert("hello" + " " + username + " " + "kindly read the sentence in the next prompt and input the appropriate answer for the command")
 alert2 = alert("There was once a _________ programmer who wanted to use Javascript to ___________ the __________ ");
 let adjective = prompt("select an adjective");
 let verb = prompt("select a verb ");
 let noun = prompt("select a noun");
+let confirm1 = confirm("are you sure of your inputs");
 alert3 = alert("kindly hold on while we process your answer");
 alert4 = alert("There was once a " + " " + adjective + " " + "programmer who wanted to use Javascript to" + " " + verb + " " + "the" + " " + noun);
 let res = adjective.concat(verb, noun);
@@ -13,8 +14,9 @@ let noun1 = noun.charAt(noun.length-1);
  let character = verb1.concat(noun1);
  let rev = [...character].reverse().join("");
  let sliceadj = adjective.slice(0, 1);
- let adjective3 = adjective.concat("!!!")
- let verb3 = verb.concat("!!!")
- let noun3 = noun.concat("!!!")
- console.log( username +" "+ adjective +" "+ verb +" "+ noun +" "+ res +" "+ cap +" "+ verb1 +" "+ noun1 +" "+ character +" "+ 
+ let adjective3 = adjective.concat("!!!");
+ let verb3 = verb.concat("!!!");
+ let noun3 = noun.concat("!!!");
+ console.log(alert4);
+ console.log(username +" "+ adjective +" "+ verb +" "+ noun +" "+ res +" "+ cap +" "+ verb1 +" "+ noun1 +" "+ character +" "+ 
  rev +" "+ sliceadj +" "+ adjective3 +" "+ verb3 +" "+ noun3 );
---
+++ readme.md
@@ -0,0 +1,5 @@
+# MY JAVASCRIPT #
+**how to prompt a user**
+* my web page
+* javascrpt
+## sources ##
